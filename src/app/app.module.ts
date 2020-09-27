77c9a1b935efad1bc84747623c0dde090e2d5eea
Daniel Poss <dpatposs@msn.com>
2020-09-27 11:56:23 -0500

    Adding header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TerminalComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
